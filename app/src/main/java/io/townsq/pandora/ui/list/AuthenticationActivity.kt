46471d5e6fc1558ff421d6745dd8bc2db0757100
Volnei <Volnei.Foss@townsq.com.br>
2023-05-31 12:57:04 -0300

    configuracao inicial

---
+++ app/src/main/java/io/townsq/pandora/ui/list/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package io.townsq.pandora.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.townsq.pandora.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
---
+++ app/src/main/java/io/townsq/pandora/ui/list/AuthenticationViewModel.kt
@@ -0,0 +1,6 @@
+package io.townsq.pandora.ui.list
+
+import androidx.lifecycle.ViewModel
+
+class AuthenticationViewModel : ViewModel() {
+}
