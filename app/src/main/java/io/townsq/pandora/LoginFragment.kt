f8f57916847e84e7f91125dfc7d28cdbe869d5ee
Volnei <Volnei.Foss@townsq.com.br>
2023-06-01 19:05:20 -0300

    finished login screen layout

---
+++ app/src/main/java/io/townsq/pandora/LoginFragment.kt
@@ -0,0 +1,20 @@
+package io.townsq.pandora
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return view
+    }
+
+}
