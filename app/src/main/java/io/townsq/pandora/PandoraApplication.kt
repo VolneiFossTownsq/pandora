ebae20485d77aec9fa5c0ffe9d121b365bdf8233
Volnei <Volnei.Foss@townsq.com.br>
2023-06-16 16:00:22 -0300

    fragment creation and layout adjustments

---
+++ app/src/main/java/io/townsq/pandora/PandoraApplication.kt
@@ -0,0 +1,23 @@
+package io.townsq.pandora
+
+import android.app.Application
+import io.townsq.pandora.ui.feed.di.recordListModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class PandoraApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@PandoraApplication)
+            modules(
+                listOf(
+                    recordListModule
+                )
+            )
+        }
+    }
+}
