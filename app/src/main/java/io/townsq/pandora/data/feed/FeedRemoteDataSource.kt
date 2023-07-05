6510146548abd05db7be3ec0be4837151794761f
Volnei <Volnei.Foss@townsq.com.br>
2023-07-05 09:52:04 -0300

    encapsulation with try catch

--- app/src/main/java/io/townsq/pandora/data/feed/FeedRemoteDataSource.kt
+++ app/src/main/java/io/townsq/pandora/data/feed/FeedRemoteDataSource.kt
@@ -6,10 +6,13 @@ import java.io.IOException
 class FeedRemoteDataSource(private val feedService: FeedService) {
 
     suspend fun getRecords(recordType: String? = null): Result<List<Record>> {
-        val response = feedService.getRecords(recordType)
-        return if (response.isSuccessful) {
-            Result.success(response.body().orEmpty())
-        } else {
+        return try {
+            val response = feedService.getRecords(recordType)
+            if (response.isSuccessful) {
+                Result.success(response.body().orEmpty())
+            }
+            Result.failure(IOException("Error on get method"))
+        } catch (e: IOException) {
             Result.failure(IOException("Error on get method"))
         }
     }
