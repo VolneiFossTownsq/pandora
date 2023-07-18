a182db8230e79d2c6323ad1b55eb7b717df73073
Volnei <Volnei.Foss@townsq.com.br>
2023-07-18 15:11:53 -0300

    vehicle get and logic to proceed to the next fragment

--- app/src/main/java/io/townsq/pandora/ui/createRecord/VehicleSelectionFragment.kt
+++ app/src/main/java/io/townsq/pandora/ui/createRecord/VehicleSelectionFragment.kt
@@ -1,29 +1,29 @@
 package io.townsq.pandora.ui.createRecord
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import io.townsq.pandora.R
 import io.townsq.pandora.adapter.CreateRecordAdapter
+import io.townsq.pandora.data.models.Vehicle
 import io.townsq.pandora.databinding.FragmentVehicleSelectionBinding
-import io.townsq.pandora.ui.feed.FeedActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class VehicleSelectionFragment : Fragment() {
 
     private var binding: FragmentVehicleSelectionBinding? = null
     private var continueToRecordType: Button? = null
     private var backToFeed: ImageView? = null
-    private val recordViewModel: RecordViewModel by viewModel()
+    private val recordViewModel: RecordViewModel by activityViewModel()
     private var recyclerViewRecord: RecyclerView? = null
-    private var createRecordAdapter: CreateRecordAdapter? = null
+    private var createRecordAdapter: CreateRecordAdapter = CreateRecordAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,41 +34,49 @@ class VehicleSelectionFragment : Fragment() {
 
         continueToRecordType = binding?.continueButtoToRecordType
         backToFeed = binding?.backToFeed
-        createRecordAdapter = CreateRecordAdapter()
         recyclerViewRecord = binding?.recyclerViewRecordVehicle
         recyclerViewRecord?.adapter = createRecordAdapter
 
         setupBindings()
-        setupView()
+        setupViews()
 
         return binding?.root
     }
 
-    fun setupBindings() {
-        recordViewModel.recordsLiveData.observe(viewLifecycleOwner) { records ->
-            createRecordAdapter?.setRecords(records)
-        }
-
-        recordViewModel.selectedItemPosition.observe(viewLifecycleOwner) { selectedPosition ->
-            createRecordAdapter?.notifyDataSetChanged()
-        }
-    }
-
-    private fun setupView() {
+    private fun setupViews() {
         continueToRecordType?.setOnClickListener {
-            onClickToRecordType()
+            onGoToRecordType()
         }
         backToFeed?.setOnClickListener {
-            onClickBackToFeed()
+            activity?.finish()
         }
     }
 
-    private fun onClickToRecordType() {
-        findNavController().navigate(R.id.action_vehicleSelectionFragment_to_recordActionFragment)
+    private fun setupBindings() {
+        recordViewModel.vehicleList.observe(viewLifecycleOwner) { vehicleList ->
+            setupAdapter(vehicleList)
+        }
     }
 
-    private fun onClickBackToFeed(){
-        val intent = Intent(requireContext(), FeedActivity::class.java)
-        startActivity(intent)
+    private fun setupAdapter(vehicleList: List<Vehicle>) {
+        with(recyclerViewRecord) {
+            this?.setHasFixedSize(true)
+            this?.adapter = createRecordAdapter
+        }
+        createRecordAdapter.setVehicleList(vehicleList)
+    }
+
+    private fun getVehicle() {
+        recordViewModel.setSelectedVehicle(createRecordAdapter.getActualVehicle())
+    }
+
+    private fun onGoToRecordType() {
+        getVehicle()
+        if (recordViewModel.selectedVehicle.value == null) {
+            Toast.makeText(requireContext(), "Escolha uma opção para continuar", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            findNavController().navigate(R.id.action_vehicleSelectionFragment_to_recordActionFragment)
+        }
     }
 }
