4ac97d4193e78cb413350f41b2404e7d6ea1ad4e
Volnei <Volnei.Foss@townsq.com.br>
2023-07-18 22:13:13 -0300

    logic and tweaks to handle incoming info

--- app/src/main/java/io/townsq/pandora/ui/recordDetails/RecordDetailsActivity.kt
+++ app/src/main/java/io/townsq/pandora/ui/recordDetails/RecordDetailsActivity.kt
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import io.townsq.pandora.R
 import io.townsq.pandora.databinding.ActivityRecordDetailsBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class RecordDetailsActivity : AppCompatActivity() {
 
+    companion object {
+        const val ARG_RECORD_ID = "recordId"
+    }
+    private val recordDetailsViewModel: RecordDetailsViewModel by viewModel {
+        parametersOf(
+            intent.extras?.getString(ARG_RECORD_ID)
+        )
+    }
     private var binding: ActivityRecordDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +25,7 @@ class RecordDetailsActivity : AppCompatActivity() {
         binding = ActivityRecordDetailsBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        recordDetailsViewModel
         supportFragmentManager.commit {
             replace(R.id.recordDetailsFragmentContainer, RecordDetailsFragment())
         }
--- app/src/main/java/io/townsq/pandora/ui/recordDetails/RecordDetailsFragment.kt
+++ app/src/main/java/io/townsq/pandora/ui/recordDetails/RecordDetailsFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import io.townsq.pandora.data.models.Record
 import io.townsq.pandora.databinding.FragmentRecordDetailsBinding
+import io.townsq.pandora.utils.DateFormat
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class RecordDetailsFragment : Fragment() {
 
     private var binding: FragmentRecordDetailsBinding? = null
-
+    private val recordDetailsViewModel: RecordDetailsViewModel by activityViewModel()
+    private var infoRecordType: TextView? = null
+    private var infoVehicleName: TextView? = null
+    private var infoDateDetails: TextView? = null
+    private var infoLicensePlateDetails: TextView? = null
+    private var infoServiceCost: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +27,27 @@ class RecordDetailsFragment : Fragment() {
     ): View? {
         binding = FragmentRecordDetailsBinding.inflate(inflater, container, false)
 
+        infoRecordType = binding?.recordType
+        infoVehicleName = binding?.carName
+        infoDateDetails = binding?.infoDate
+        infoLicensePlateDetails = binding?.plateNumber
+        infoServiceCost = binding?.serviceValue
+
+        setupBindings()
         return binding?.root
+    }
+
+    private fun setupBindings() {
+        recordDetailsViewModel.record.observe(viewLifecycleOwner) { record ->
+            onRecordDataFetched(record)
+        }
+    }
+
+    private fun onRecordDataFetched(record: Record?) {
+        infoRecordType?.text = record?.recordType?.type
+        infoVehicleName?.text = record?.vehicle?.name
+        infoDateDetails?.text = DateFormat().formatDate(record?.recordDate ?: "")
+        infoLicensePlateDetails?.text = record?.vehicle?.licensePlate
+        infoServiceCost?.text = "$${record?.serviceCost ?: "0"}"
     }
 }
